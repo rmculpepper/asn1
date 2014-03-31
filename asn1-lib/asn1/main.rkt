73a52762eee08ead68f8a4d24efdba2196c1ce62
Ryan Culpepper <ryanc@racket-lang.org>
2014-03-31 12:53:32 -0400

    add sequence<->list conversion utils

--- asn1-lib/asn1/main.rkt
+++ asn1-lib/asn1/main.rkt
@@ -62,6 +62,12 @@
 
          define-asn1-type
 
+         (contract-out
+          [list->sequence
+           (->* [any/c (listof symbol?)] [(listof symbol?)] any)]
+          [sequence->list
+           (->* [any/c (listof symbol?)] [(listof symbol?)] any)])
+
          ;; private/base-types.rkt
          printable-string?
          ia5string?
@@ -258,3 +264,51 @@
    [(define-asn1-type name:id type)
     #:declare type (expr/c #'asn1-type?)
     #'(define name (Delay type.c))]))
+
+;; ============================================================
+
+;; list->sequence and sequence->list
+;; List must contain all mandatory fields and a *prefix* of optional fields.
+(define (list->sequence v0 mand-fields [opt-fields null])
+  (define mand-field-count (length mand-fields))
+  (define (loop fields v index)
+    (cond [(and (pair? fields) (pair? v))
+           (cons (list (car fields) (car v))
+                 (loop (cdr fields) (cdr v) (add1 index)))]
+          [(pair? fields)
+           (if (< index mand-field-count) ;; mandatory
+               (error 'list->sequence
+                      "missing mandatory field\n  field: ~e\n  value: ~e"
+                      (car fields) v0)
+               (loop (cdr fields) v (add1 index)))]
+          [(pair? v)
+           (error 'list->sequence
+                  "too many values in list\n  extra values: ~e"
+                  (car v))]
+          [else null]))
+  (cons 'sequence (loop (append mand-fields opt-fields) v0 0)))
+
+(define (sequence->list v mand-fields [opt-fields null])
+  (define mand-field-count (length mand-fields))
+  (define (loop lvs fields index first-missing-field)
+    (cond [(pair? fields)
+           (cond [(assq (car fields) lvs)
+                  => (lambda (lv)
+                       (when first-missing-field
+                         (error 'sequence->list
+                                (string-append "present field after missing optional field"
+                                               "\n  missing: ~e\n  present: ~e\n  value: ~e")
+                                first-missing-field (car fields) v))
+                       (cons (cadr lv)
+                             (loop lvs (cdr fields) (add1 index) #f)))]
+                 [(< index mand-field-count) ;; mandatory
+                  (error 'sequence->list
+                         "missing mandatory field\n  field: ~e\n  value: ~e"
+                         (car fields) v)]
+                 [else
+                  (loop lvs (cdr fields) (add1 index) (car fields))])]
+          [else null]))
+  (match v
+    [(cons 'sequence (and lvs (list (list (? symbol?) _) ...)))
+     (loop lvs (append mand-fields opt-fields) 0 #f)]
+    [_ (error 'sequence->list "ill-formed sequence value\n  value: ~e" v)]))
