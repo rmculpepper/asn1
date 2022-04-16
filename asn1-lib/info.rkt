8808d23025a64d56fcadb8a1403cdb960aeaee63
Ryan Culpepper <ryanc@racket-lang.org>
2022-04-16 21:05:21 +0200

    bump version to 1.3

--- asn1-lib/info.rkt
+++ asn1-lib/info.rkt
@@ -6,7 +6,7 @@
 ;; ========================================
 ;; pkg info
 
-(define version "1.2")
+(define version "1.3")
 (define collection "asn1")
 (define deps '("base" ["binaryio-lib" #:version "1.1"]))
 (define pkg-authors '(ryanc))
